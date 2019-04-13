1bc0923b43af95afdaca3fe725c33f7412235669
Geoff Golder <geoffgolder@gmail.com>
2019-04-13 13:21:59 -0500

    better

--- setup.py
+++ setup.py
@@ -1,26 +1,10 @@
-from setuptools import setup, find_packages
-import sys, os
-
-version = '0.0.1'
+from setuptools import setup
 
 setup(name='jbot',
-      version=version,
-      description="All the code Jeremey builds during the Foundations of DL course.  A Neural Net / Deep Learning / Pytorch toolkit",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='Jeremy Howard',
-      author_email='',
-      url='fast.ai',
-      license='MIT',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+        version="0.0.1",
+        description=f"jeremy howard's bag of tricks",
+        url='http://fast.ai',
+        author='jeremy howard, curated by geoff golder',
+        license='mit',
+        packages=['hellogeoff'],
+        zip_safe=True)
